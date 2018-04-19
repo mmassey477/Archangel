ff5d3874ee481be14e3f01e875030123871119a5
kyleschultz <kysc9699@colorado.edu>
2018-04-19 11:42:04 -0600

    Added user verification

---
+++ Site/HTML/LoginAndSignUp/public/app.js
@@ -0,0 +1,35 @@
+const CreateUser = document.querySelector('.CreateUser')
+CreateUser.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const username = CreateUser.querySelector('.username').value
+  const password = CreateUser.querySelector('.password').value
+  post('/createUser', { username, password })
+})
+const Login = document.querySelector('.Login')
+Login.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const username = Login.querySelector('.username').value
+  const password = Login.querySelector('.password').value
+  post('/login', { username, password })
+    .then(({ status }) => {
+      if (status === 200) {
+        alert('login success')
+        next()
+      }
+      else alert('login failed')
+    })
+})
+function post (path, data) {
+  return window.fetch(path, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
+
+function next (){
+  window.location = "127.0.0.1:8080/main.html";
+}
