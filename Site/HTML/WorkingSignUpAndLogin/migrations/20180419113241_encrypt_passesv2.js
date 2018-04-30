8af5f2b3e34fd87de477bdd90ce8112bccbe211a
kyleschultz <kysc9699@colorado.edu>
2018-04-30 13:29:08 -0600

    Latest version of site - Kyle

--- Site/HTML/WorkingSignUpAndLogin/migrations/20180419113241_encrypt_passesv2.js
+++ Site/HTML/WorkingSignUpAndLogin/migrations/20180419113241_encrypt_passesv2.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-  return knex.schema.createTableIfNotExists('user', function (t) {
+  return knex.schema.createTable('user', function (t) {
     t.increments('id').primary()
     t.string('username').notNullable()
     t.string('password').notNullable()
